12dfebfbb734bd115e83f7484fabcbc59fc24f2a
Rich Burdon <burdon@burdon-mbp-2020.local>
2020-03-28 20:48:47 -0400

    Part/port table.

--- pages/api/apps.js
+++ pages/api/apps.js
@@ -21,9 +21,31 @@ export default async (req, res) => {
   try {
     switch (command) {
       case 'start': {
-        // TODO(burdon): Requires nginx config?
-        const path = searchParams.get('path') || name.substring(name.indexOf('/'));
-        const port = searchParams.get('port') || 8000;
+        // TODO(burdon): Currently paths and ports are hard-coded in the apache config.
+        // TODO(burdon): The path isn't required since the frontend proxy maps the external path to the server's port.
+        // Otherwise, the registry record must contain the path information.
+        const routes = {
+          'wireline.io/editor': {
+            path: '/editor',
+            port: 8080
+          },
+          'wireline.io/todo': {
+            path: '/todo',
+            port: 8081
+          },
+          'wireline.io/kanban': {
+            path: '/kanban',
+            port: 8082
+          },
+          'wireline.io/chess': {
+            path: '/chess',
+            port: 8082
+          }
+        };
+
+        const route = routes[name];
+        const path = route ? route.path : searchParams.get('path') || '/';
+        const port = route ? route.port : searchParams.get('port') || 8100;   // Default test port.
 
         const args = [
           'app', 'serve',
@@ -32,7 +54,10 @@ export default async (req, res) => {
           '--port', port
         ];
 
-        result = await exec('wire', { args, detached: true });
+        console.log('::::::::::', JSON.stringify(args));
+
+        // TODO(burdon): Parse PID (e.g., "[1] 94685").
+        result = await exec('wire', { args, detached: false });
         break;
       }
 
