0dca3a46792bde1c6f6c4d7b1807bb62c2069943
Rich Burdon <rich.burdon@gmail.com>
2020-03-30 18:11:52 -0400

    Dynamic conf dep on NODE_ENV (#24)
    Co-authored-by: Rich Burdon <burdon@burdon-mbp-2020.local>

--- next.config.js
+++ next.config.js
@@ -3,8 +3,11 @@
 //
 
 const path = require('path');
+const webpack = require('webpack');
 const withImages = require('next-images');
 const VersionFile = require('webpack-version-file-plugin');
+
+const CONFIG_FILE = process.env.NODE_ENV === 'development' ? 'config-dev' : 'config-prod';
 
 module.exports = withImages({
   webpack(config) {
@@ -16,6 +19,12 @@ module.exports = withImages({
     );
 
     config.plugins.push(
+      // Define the build config file based on the target.
+      // https://webpack.js.org/plugins/normal-module-replacement-plugin
+      new webpack.NormalModuleReplacementPlugin(/(.*)__CONFIG_FILE__/, (resource) => {
+        resource.request = resource.request.replace(/__CONFIG_FILE__/, CONFIG_FILE);
+      }),
+
       new VersionFile({
         packageFile: path.join(__dirname, 'package.json'),
         outputFile: path.join(__dirname, 'version.json')
