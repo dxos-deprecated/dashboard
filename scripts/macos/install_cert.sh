523db3f958c4348a38e8b1be3d26022367dad8be
telackey <telackey@bozemanpass.com>
2020-04-13 22:48:43 -0500

    Scripts for local dev on MacOS. (#43)
    * Scripts for local dev on MacOS.
    
    * Back to xbox-ssl.conf name.
    
    * Hack fix for /api

---
+++ scripts/macos/install_cert.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#
+# Install Cert.
+# https://apple.stackexchange.com/questions/80623/import-certificates-into-the-system-keychain-via-the-command-line
+# https://derflounder.wordpress.com/2011/03/13/adding-new-trusted-root-certificates-to-system-keychain/
+# OS/X Keychain Access
+#
+
+CERT="${1:-xbox.local.crt}"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain ${CERT}
+fi
