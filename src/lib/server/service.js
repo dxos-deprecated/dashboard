4e3c36152886ba0e3935c6dc3289452780cf3913
Egor Gripasov <gripasovegor@gmail.com>
2020-04-18 11:35:10 -0400

    start of dxos services.

---
+++ src/lib/server/service.js
@@ -0,0 +1,35 @@
+//
+// Copyright 2020 DxOS
+//
+
+// import debug from 'debug';
+// import { spawn } from 'child_process';
+
+// const log = debug('dxos:dashboard:service');
+
+export class Service {
+  constructor(name, config) {
+    this._name = name;
+    this._config = config;
+  }
+
+  // async start() {
+
+  // }
+
+  // async status() {
+
+  // }
+
+  // async stop() {
+
+  // }
+
+  // async restart() {
+
+  // }
+
+  // async logs() {
+
+  // }
+}
